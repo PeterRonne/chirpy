18d7496c76229fd9038765e719f6ff196595274c
PeterRonne <peter.roest07@gmail.com>
2025-07-12 00:53:05 +0200

    add file server

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	fmt.Fprintln(w, "Welcome til min server")
-	// })
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
